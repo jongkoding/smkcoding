c412e91560be58ded915b0df32d6c73e355dcc97
saskiaanjani <wahyudianjani54@gmail.com>
2023-10-26 12:55:43 +0700

    push tugas ecommerce smkcoding

---
+++ components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer class="w-full border-t border-slate-200 py-6">
+    <div class="container">
+    <div class="flex justify-center items-center">
+    </div>
+    </div>
+    </footer>
+    </template>
